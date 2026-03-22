b666e1ba1be50f3ef2e6f066d9e5702c4cd9ed03
kurino <kurino0227@gmail.com>
2026-03-22 09:57:35 +0900

    通常ボタンの作成

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,7 @@ import ButtonSample from "@/components/ui/ButtonSample";
 import CardSample from "@/components/ui/CardSample";
 import TwoColumnSample from "@/components/layout/TwoColumnSample";
 import MenuSample from "@/components/ui/MenuSample";
+import Button from "@/components/ui/Button";
 
 export default function About() {
   const boxClass = "box";
@@ -9,6 +10,7 @@ export default function About() {
   return (
     <TwoColumnSample>
       <div className={boxClass}>
+        <Button href="#" text="導入事例をすべて見る" />
         <p style={{ color: "#333", backgroundColor: "pink" }}>
           段落が入ります。段落が入ります。
           <br />
