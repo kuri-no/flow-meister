b5ea73c2572db934eaa1607cdbde1ccb7a68108d
kurino <kurino0227@gmail.com>
2026-03-22 11:32:28 +0900

    見出しの作成

--- src/components/layout/TwoColumnSample.js
+++ src/components/layout/TwoColumnSample.js
@@ -1,7 +1,7 @@
 export default function TwoColumnSample({ children }) {
   return (
     <div style={{display: "grid", gridTemplateColumns: "1fr 200px" }}>
-      <div style={{ color: "#333", backgroundColor: "orange", padding: "40px" }}>
+      <div style={{ color: "#333", backgroundColor: "white", padding: "40px" }}>
         {children}
       </div>
       <div style={{ color: "#333", backgroundColor: "pink", padding: "40px" }}>
