2a9b6fc14aa9a5155de98adba09d66aa48eab725
kurino <kurino0227@gmail.com>
2026-05-03 11:05:20 +0900

    ラジオボタンの追加

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ import PageFirstView from "@/components/layout/PageFirstView";
 import TwoColumn from "@/components/layout/TwoColumn";
 import Input from "@/components/ui/Input";
 import Textarea from "@/components/ui/Textarea";
+import Radio from "@/components/ui/Radio";
 
 export default function Contact() {
   return (
@@ -30,6 +31,15 @@ export default function Contact() {
           placeholder="お伝え事項がございましたらご自由にご記入ください。"
           required={true}
         ></Textarea>
+        <p>お問い合わせ用のページです</p>
+        <Radio
+          name="color"
+          items={[
+            { value: "青", text: "青" },
+            { value: "赤", text: "赤" },
+            { value: "黄", text: "黄" },
+          ]}
+        ></Radio>
       </TwoColumn>
     </>
   );
