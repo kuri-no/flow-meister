c1b1bcf0cc98869ab23f905e6b2beefe49e7487c
kurino <kurino0227@gmail.com>
2026-05-06 11:55:41 +0900

    お問い合わせページの作成

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,7 @@
 import PageFirstView from "@/components/layout/PageFirstView";
 import OneColumn from "@/components/layout/OneColumn";
-import Input from "@/components/ui/Input";
-import Textarea from "@/components/ui/Textarea";
-import Radio from "@/components/ui/Radio";
-import Checkbox from "@/components/ui/Checkbox";
-import Submit from "@/components/ui/Submit";
-import Label from "@/components/ui/Label";
 import Form from "@/components/layout/Form";
+import styles from "./Contact.module.css";
 
 export default function Contact() {
   return (
@@ -17,48 +12,11 @@ export default function Contact() {
         textAlign="isCenter"
       />
       <OneColumn isSlim={true}>
+        <p className={styles.text}>
+          サービスに関するご相談やお問い合わせは、必要事項を入力の上、お問い合わせください。<br />
+          2～3営業日以内に、担当よりメールにて返信いたします。
+        </p>
         <Form />
-        <p>お問い合わせ用のページです</p>
-        <Label text="会社・組織名" isRequired={true} id="company" />
-        <Input
-          id="company"
-          type="text"
-          name="company"
-          defaultValue=""
-          placeholder="例：株式会社xxx"
-          required={true}
-          pattern="xxx"
-        />
-        <p>お問い合わせ用のページです</p>
-        <Textarea
-          id="message"
-          name="message"
-          defaultValue=""
-          placeholder="お伝え事項がございましたらご自由にご記入ください。"
-          required={true}
-        />
-        <p>お問い合わせ用のページです</p>
-        <Radio
-          name="color"
-          items={[
-            { value: "青", text: "青" },
-            { value: "赤", text: "赤", cheked: true },
-            { value: "黄", text: "黄" },
-          ]}
-          required={true}
-        />
-        <p>お問い合わせ用のページです</p>
-        <Checkbox
-          name="color2"
-          items={[
-            { value: "青", text: "青" },
-            { value: "赤", text: "赤", cheked: true },
-            { value: "黄", text: "黄" },
-          ]}
-          required={true}
-        />
-        <p>お問い合わせ用のページです</p>
-        <Submit text="送信する" />
       </OneColumn>
     </>
   );
